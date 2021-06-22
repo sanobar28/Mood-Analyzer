16195420ab7ba185402cc5c09eb75b08ba3125e5
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-22 10:43:23 +0530

    added: Handles null value exception

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -1,19 +1,27 @@
 package moodanalyzer;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
 
-	
 	/**
 	 * Test case for sad mood
 	 * 
 	 */
 	@Test
 	public void givenMessgae_WhenSad_ShouldReturn_Sad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood"); //passed message to constructor
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
 		String mood = moodAnalyzer.analyseMood();
+		try {
+			moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // passed message to constructor
+		
 		Assert.assertEquals("SAD", mood);
 	}
 
@@ -23,9 +31,29 @@ public class MoodAnalyzerTest {
 	 */
 	@Test
 	public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood"); //passed message to constructor
+		
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
 		String mood = moodAnalyzer.analyseMood();
+		try {
+			moodAnalyzer = new MoodAnalyzer("I am in any Mood");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		Assert.assertEquals("HAPPY", mood);
+	}
+
+	/**
+	 * UC-2 Handles Null value Exception
+	 */
+	@Test
+	public void givenNullMood() {
+		MoodAnalyzer moodAnalyzer;
+		String mood;
+		try {
+			moodAnalyzer = new MoodAnalyzer(null);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 	}
 
 }
